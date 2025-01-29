2c022f4a0a3079cecd1df1dd6e1fc6c153a6f583
vm <iankcoffey@gmail.com>
2025-01-29 09:45:40 -0500

    syncing Si430

---
+++ udp_spoof/spoof.py
@@ -0,0 +1,42 @@
+#!usr/bin/python3
+
+from socket import *
+import struct
+raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
+udp_payload = b"Hello World\n" 
+                  #UDP HEADER#
+#####################################################
+#                        |                          #
+#      #SRC PORT         |           #DST PORT      #
+#------------------------|--------------------------# 
+#      #LENGTH           |           #CHECKSUM      # 
+#                        |                          #
+#####################################################
+# pack srcPort, dstPort, length, & checkSum into header
+# 4 unsigned shorts, because shorts are 2 bytes, similar to hex
+# 8 + UDP len to account for header
+udp_header = struct.pack(">4H",12345, 9000, 8 + len(udp_payload), 0)
+ip_payload = udp_header + udp_payload
+
+
+
+# IP vars
+version = 4
+ihl = 5 
+version_ihl = (version << 4) | ihl # put version and ihl in one byte
+type_of_service = 0
+total_length = 20 + len(ip_payload)
+ip_header = struct.pack(">BBH", version_ihl, type_of_service, total_length)
+
+# Pack more stuff 
+ip_header += struct.pack(">HH", 12345, 0)
+ttl = 64
+protocol = 17
+checksum = 0
+ip_header += struct.pack(">BBH", ttl, protocol, checksum)
+
+# Pack source and destination address
+ip_header += struct.pack(">4B", 192,168,42,8) # src
+ip_header += struct.pack(">4B", 10,0,2,15) # dest (target)
+ip_pkt = ip_header + ip_payload
+
